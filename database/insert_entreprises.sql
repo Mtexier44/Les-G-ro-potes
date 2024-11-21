54a9d5934dcafab23e102d0e173fc07ec3257a2e
Christianngono <christianngonoabanda384@gmail.com>
2024-11-21 13:11:38 +0100

    To commit insert_entreprises.sql

---
+++ database/insert_entreprises.sql
@@ -0,0 +1,3 @@
+INSERT INTO entreprise (nom, type_entreprise, email, telephone, adresse, 
+villes_desservies, nombre_patients, description_services, logo, photos, videos)
+VALUES ('Entreprise 1', 'Assistance à domicile', 'contact@entreprise1.com', '0123456789', '123 Rue Cameroun, Paris', 'Paris, Lyon', 50, 'Services aide à la personne', 'logo1.png', 'photo1.jpg,photo2.jpg', 'video1.mp4');
