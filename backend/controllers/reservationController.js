b6887985d3572665663811a04dd419322326d0a0
Christianngono <christianngonoabanda384@gmail.com>
2024-12-11 14:03:07 +0100

    To add again reservationController.js

--- backend/controllers/reservationController.js
+++ backend/controllers/reservationController.js
@@ -2,7 +2,7 @@ const db = require ('../config/db');
 
 // Récupérer toutes les réservations
 exports.getAllReservations = (req, res) => {
-    const sql = 'SELECT * FROM reservations';
+    const sql = 'SELECT * FROM reservation';
     db.all(sql, [], (err, rows) => {
         if (err) {
             return res.status(500).json({ message: 'Error fetching reservations', error: err.message });
@@ -15,7 +15,7 @@ exports.getAllReservations = (req, res) => {
 exports.createReservation = (req, res) => {
     const { id, utilisateur_id, service_id, date_reservation, status } = req.body;
 
-    const sql = 'INSERT INTO reservations (id, utilisateur_id, service_id, date_reservation, status) VALUES (?,?,?,?,?)';
+    const sql = 'INSERT INTO reservation (id, utilisateur_id, service_id, date_reservation, status) VALUES (?,?,?,?,?)';
     db.run(sql, [id, utilisateur_id, service_id, date_reservation, status], function (err) {
         if (err) {
             return res.status(500).json({ message: 'Error creating reservation', error: err.message });
@@ -27,7 +27,7 @@ exports.createReservation = (req, res) => {
 // Récupérer une réservation par son ID
 exports.getReservationById = (req, res) => {
     const { id } = req.params;
-    const sql = 'SELECT * FROM reservations WHERE id =?';
+    const sql = 'SELECT * FROM reservation WHERE id =?';
     db.get(sql, [id], (err, row) => {
         if (err) {
             return res.status(500).json({ message: 'Error fetching reservation', error: err.message });
@@ -42,7 +42,7 @@ exports.getReservationById = (req, res) => {
 // Récupérer une réservation par rapport l'utilisateur_id
 exports.getReservationByUtilisateurId = (req, res) => {
     const { utilisateur_id } = req.params;
-    const sql = 'SELECT * FROM reservations WHERE utilisateur_id =?';
+    const sql = 'SELECT * FROM reservation WHERE utilisateur_id =?';
     db.all(sql, [utilisateur_id], (err, rows) => {
         if (err) {
             return res.status(500).json({ message: 'Error fetching reservations', error: err.message });
@@ -54,7 +54,7 @@ exports.getReservationByUtilisateurId = (req, res) => {
 // Récupérer les réservations par rapport au service_id
 exports.getReservationByServiceId = (req, res) => {
     const { service_id } = req.params;
-    const sql = 'SELECT * FROM reservations WHERE service_id =?';
+    const sql = 'SELECT * FROM reservation WHERE service_id =?';
     db.all(sql, [service_id], (err, rows) => {
         if (err) {
             return res.status(500).json({ message: 'Error fetching reservations', error: err.message });
@@ -66,7 +66,7 @@ exports.getReservationByServiceId = (req, res) => {
 // Modifier une réservation d'utilisateur
 exports.updateReservationByUtilisateurId = (req, res) => {
     const { id, utilisateur_id, service_id, date_reservation, status } = req.body;
-    const sql = 'UPDATE reservations SET utilisateur_id =?, service_id =?, date_reservation =?, status =? WHERE id =?';
+    const sql = 'UPDATE reservation SET utilisateur_id =?, service_id =?, date_reservation =?, status =? WHERE id =?';
     db.run(sql, [utilisateur_id, service_id, date_reservation, status, id], (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error updating reservation', error: err.message });
@@ -78,7 +78,7 @@ exports.updateReservationByUtilisateurId = (req, res) => {
 // Supprimer une réservation par son ID
 exports.deleteReservationById = (req, res) => {
     const { id } = req.params;
-    const sql = 'DELETE FROM reservations WHERE id =?';
+    const sql = 'DELETE FROM reservation WHERE id =?';
     db.run(sql, [id], (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error deleting reservation', error: err.message });
@@ -90,7 +90,7 @@ exports.deleteReservationById = (req, res) => {
 // Supprimer toutes les réservations pour un utilisateur
 exports.deleteAllReservationsByUtilisateurId = (req, res) => {
     const { utilisateur_id } = req.params;
-    const sql = 'DELETE FROM reservations WHERE utilisateur_id =?';
+    const sql = 'DELETE FROM reservation WHERE utilisateur_id =?';
     db.run(sql, [utilisateur_id], (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error deleting all reservations', error: err.message });
@@ -102,7 +102,7 @@ exports.deleteAllReservationsByUtilisateurId = (req, res) => {
 // Supprimer toutes les réservations pour un service_id
 exports.deleteAllReservationsByServiceId = (req, res) => {
     const { service_id } = req.params;
-    const sql = 'DELETE FROM reservations WHERE service_id =?';
+    const sql = 'DELETE FROM reservation WHERE service_id =?';
     db.run(sql, [service_id], (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error deleting all reservations', error: err.message });
@@ -113,7 +113,7 @@ exports.deleteAllReservationsByServiceId = (req, res) => {
 
 // Supprimer toutes les réservations
 exports.deleteAllReservations = (req, res) => {
-    const sql = 'DELETE FROM reservations';
+    const sql = 'DELETE FROM reservation';
     db.run(sql, [], (err) => {
         if (err) {
             return res.status(500).json({ message: 'Error deleting all reservations', error: err.message });
