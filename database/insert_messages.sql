2451e6a75b6a7c8cdf16fa9141c08ddfb5258df4
Christianngono <christianngonoabanda384@gmail.com>
2024-12-11 09:34:52 +0100

    To add insert_messages.sql

---
+++ database/insert_messages.sql
@@ -0,0 +1,2 @@
+INSERT INTO message (utilisateur_id, entreprise_id, message, date_envoi, lu)
+VALUES (1, 1, 'Bonjour, je souhaiterais avoir plus des informations sur vos services.', '2024-11-20 09:00:00', FALSE);
