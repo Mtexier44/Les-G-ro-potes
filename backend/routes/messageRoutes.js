080939de3188c6e5609a286d23acc71b3ed3569a
Christianngono <christianngonoabanda384@gmail.com>
2025-03-19 16:47:17 +0100

    To put messageRoutes.js

--- backend/routes/messageRoutes.js
+++ backend/routes/messageRoutes.js
@@ -1,11 +1,32 @@
 const express = require('express');
-const { getMessages, getMessageById, createMessage, updateMessage, deleteMessage } = require('../controllers/messageController');
 const router = express.Router();
+const messageController = require('../controllers/messageController');
 
-router.get('/', getMessages);
-router.get('/:id', getMessageById);
-router.post('/', createMessage);
-router.put('/:id', updateMessage);
-router.delete('/:id', deleteMessage);
+// Route pour ajouter un message
+router.post('/', messageController.addMessage);
+
+// Route pour obtenir tous les messages d'une entreprise
+router.get('/entreprise/:entreprise_id', messageController.getMessagesByEntreprise);
+
+// Route pour obtenir un message par son ID
+router.get('/:id', messageController.getMessageById);
+
+// Route pour obtenir tous les messages d'un utilisateur
+router.get('/utilisateur/:utilisateur_id', messageController.getMessageByUtilisateur);
+
+// Route pour obtenir un message par date d'envoi
+router.get('/date_envoi/:date_envoi', messageController.getMessageByDateEnvoi);
+
+// Route pour obtenir les messages lus
+router.get('/lu', messageController.getMessageLu);
+
+// Route pour marquer un message comme lu
+router.put('/lu/:id', messageController.markMessageAsLu);
+
+// Route pour mettre à jour un message
+router.put('/:id', messageController.updateMessage);
+
+// Route pour supprimer un message
+router.delete('/:id', messageController.deleteMessage);
 
 module.exports = router;
