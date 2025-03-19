f664ed1e330968a66519a7fcf6fec496e75ed606
Christianngono <christianngonoabanda384@gmail.com>
2025-03-19 16:31:04 +0100

    To add again Message.js

--- backend/models/Message.js
+++ backend/models/Message.js
@@ -25,4 +25,91 @@ const getMessagesByEntreprise = (entreprise_id, callback) => {
     });
 };
 
-module.exports = { addMessage, getMessagesByEntreprise };
+// Obtenir un message par son ID
+const getMessageById = (message_id, callback) => {
+    const query = `SELECT * FROM message WHERE id =?`;
+    db.get(query, [message_id], (err, row) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, row);
+    });
+};
+
+// Fonction pour obtenir un message par utilisateur_id
+const getMessageByUtilisateur = (utilisateur_id, callback) => {
+    const query = `SELECT * FROM message WHERE utilisateur_id =?`;
+    db.all(query, [utilisateur_id], (err, rows) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, rows);
+    });
+};
+
+// Obtenir un message par sa date_envoi
+const getMessageByDateEnvoi = (date_envoi, callback) => {
+    const query = `SELECT * FROM message WHERE date_envoi =?`;
+    db.all(query, [date_envoi], (err, rows) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, rows);
+    });
+};
+
+// Obtenir le messages lu
+const getMessageLu = (callback) => {
+    const query = `SELECT * FROM message WHERE lu = 1`;
+    db.all(query, [], (err, rows) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, rows);
+    });
+};
+
+// Fonction pour marquer un message comme lu
+const markMessageAsLu = (message_id, callback) => {
+    const query = `UPDATE message SET lu = 1 WHERE id =?`;
+    db.run(query, [message_id], (err) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null);
+    });
+};
+
+// Fonction pour mettre à jour du message
+const updateMessage = (message_id, message_text, callback) => {
+    const query = `UPDATE message SET message =? WHERE id =?`;
+    db.run(query, [message_text, message_id], (err) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, this.changes); // Renvoie le nombre de lignes mises à jour
+    });
+};
+
+// Fonction pour supprimer un message
+const deleteMessage = (message_id, callback) => {
+    const query = `DELETE FROM message WHERE id =?`;
+    db.run(query, [message_id], (err) => {
+        if (err) {
+            return callback(err);
+        }
+        callback(null, this.changes); // Renvoie le nombre de lignes supprimées
+    });
+};
+
+module.exports = {
+    addMessage, 
+    getMessagesByEntreprise,
+    getMessageById,
+    getMessageByUtilisateur,
+    getMessageByDateEnvoi,
+    getMessageLu,
+    markMessageAsLu,
+    updateMessage,
+    deleteMessage
+};
