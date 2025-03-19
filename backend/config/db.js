d81fc0b3a3f42cc1c089a3e47db76a7206b3204f
Christianngono <christianngonoabanda384@gmail.com>
2025-03-19 10:54:07 +0100

    To add config/

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,16 +1,16 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Chemin vers la base de données SQLite
-const dbpath = path.join(__dirname, '..', 'database', 'create_tables.db');
+// Définir le chemin vers la base de données SQLite
+const dbPath = path.join(__dirname, '../database/create_tables.db');
 
-// Créer une instance de la base de données SQLite
-const db = new sqlite3.Database(dbpath, (err) => {
+// Créer une connexion à la base de données SQLite
+const db = new sqlite3.Database(dbPath, (err) => {
     if (err) {
-        console.error('Error connecting to the database:', err.message);
-    }else {
-        console.log('Connected to the SQLite database.');
+        console.error('Erreur lors de la connexion à la base de données SQLite:', err.message);
+        process.exit(1);
     }
+    console.log('Connexion réussie à la base de données SQLite.');
 });
 
 module.exports = db;
