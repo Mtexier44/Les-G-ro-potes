cd3e8c4f11af89a429da0b8d4fd524bec6337225
Christianngono <christianngonoabanda384@gmail.com>
2025-03-19 14:50:50 +0100

    To add serviceRoutes.js

--- backend/routes/serviceRoutes.js
+++ backend/routes/serviceRoutes.js
@@ -1,11 +1,35 @@
 const express = require('express');
-const { getAllServices, getServiceById, createService, updateService, deleteService } = require('../controllers/serviceController');
 const router = express.Router();
+const serviceController = require('../controllers/serviceController');
 
-router.get('/', getAllServices);
-router.get('/:id', getServiceById);
-router.post('/', createService);
-router.put('/:id', updateService);
-router.delete('/:id', deleteService);
+// Route pour ajouter un service
+router.post('/', serviceController.addService);
+
+// Route pour obtenir tous les services d'une entreprise
+router.get('/entreprise/:entreprise_id', serviceController.getServicesByEntreprise);
+
+// Route pour obtenir un service par son ID
+router.get('/:service_id', serviceController.getServiceById);
+
+// Route pour obtenir un service par l'ID de l'entreprise
+router.get('/entreprise-id/:entreprise_id', serviceController.getServiceByEntrepriseId);
+
+// Route pour obtenir un service par son type
+router.get('/type/:type_service', serviceController.getServiceByTypeService);
+
+// Route pour obtenir un service par sa description
+router.get('/description/:description', serviceController.getServiceByDescription);
+
+// Route pour obtenir un service par son prix
+router.get('/prix/:prix', serviceController.getServiceByPrix);
+
+// Route pour obtenir un service par sa localisation
+router.get('/localisation/:localisation', serviceController.getServiceByLocalisation);
+
+// Route pour mettre à jour un service en modifiant tous ses attributs
+router.put('/:service_id', serviceController.updateService);
+
+// Route pour supprimer un service
+router.delete('/:service_id', serviceController.deleteService);
 
 module.exports = router;
